868cd94e0f6323cf18c5fa8293c8cd2f071da7e7
HAYASHI Naoya <fuzakebito@fuzake.io>
2026-04-15 10:54:37 +0900

    feat: declare CLI tool packages

--- home/packages.nix
+++ home/packages.nix
@@ -1,1 +1,44 @@
-{ ... }: { }
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    # Core utilities
+    bat
+    eza
+    fd
+    fzf
+    ripgrep
+    jq
+    less
+
+    # System monitoring
+    bottom    # btm command
+    htop
+    duf
+    dust
+
+    # Git related
+    git
+    git-lfs
+    ghq
+
+    # Development tools
+    deno
+    pnpm
+    uv
+    mise
+    efm-langserver
+
+    # Benchmarking
+    hyperfine
+
+    # Miscellaneous
+    fastfetch
+    zoxide
+    unzip
+    pigz
+    gnuplot
+    chawan
+    libqalculate
+  ];
+}
