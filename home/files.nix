f5406f00c08bccdfbb6d595e1f9ee71b03ad997c
HAYASHI Naoya <fuzakebito@fuzake.io>
2026-04-15 11:17:12 +0900

    feat: add remaining dotfiles and configs
    - tmux: programs.tmux with extraConfig
    - bashrc: home.file
    - latexmkrc: home.file
    - mise: xdg.configFile with recursive=true
    - xremap: xdg.configFile with recursive=true
    - bin scripts: serve.ts and uqzaiko

--- home/files.nix
+++ home/files.nix
@@ -1,1 +1,31 @@
-{ ... }: { }
+{ pkgs, ... }:
+
+{
+  # tmux
+  programs.tmux = {
+    enable = true;
+    extraConfig = builtins.readFile ./files/tmux.conf;
+  };
+
+  # bashrc
+  home.file.".bashrc".source = ./files/bashrc;
+
+  # LaTeX config
+  home.file.".latexmkrc".source = ./files/latexmkrc;
+
+  # mise config
+  xdg.configFile."mise" = {
+    source = ./files/mise;
+    recursive = true;
+  };
+
+  # xremap config
+  xdg.configFile."xremap" = {
+    source = ./files/xremap;
+    recursive = true;
+  };
+
+  # bin scripts
+  home.file.".local/bin/serve.ts".source = ./files/bin/serve.ts;
+  home.file.".local/bin/uqzaiko".source = ./files/bin/uqzaiko;
+}
