4e261c05e32f3a466eb2e983bc55d2560fad5f63
HAYASHI Naoya <fuzakebito@fuzake.io>
2026-04-15 11:15:09 +0900

    feat: add terminal emulator configs

--- home/terminals.nix
+++ home/terminals.nix
@@ -1,1 +1,22 @@
-{ ... }: { }
+{ pkgs, ... }:
+
+{
+  # Alacritty
+  programs.alacritty = {
+    enable = true;
+  };
+  xdg.configFile."alacritty/alacritty.toml".source = ./files/alacritty.toml;
+
+  # Foot terminal  
+  programs.foot = {
+    enable = true;
+  };
+  xdg.configFile."foot/foot.ini".source = ./files/foot.ini;
+
+  # Wezterm (no programs.wezterm module available in this home-manager version)
+  home.packages = [ pkgs.wezterm ];
+  xdg.configFile."wezterm" = {
+    source = ./files/wezterm;
+    recursive = true;
+  };
+}
