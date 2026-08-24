ec3b8df84902a4478ab96d57a82350f4b47ce14d
HAYASHI Naoya <fuzakebito@fuzake.io>
2026-08-25 05:40:01 +0900

    fix(ssh): stop managing authorized keys

--- home/ssh.nix
+++ home/ssh.nix
@@ -39,10 +39,6 @@
   home.file.".ssh/id_rsa.pub".text = ''
     ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDa/f5WLzYFj+R4D0BLlboZB3i6BjHvfNhDr6TchGlMRboLkK2RBiBr/ek4iELT0qdBD6EMSchD5NDpoUul8FwPGPdTGav7ZPBg+MWrAtPyDqULfomDMsFE5BuAOZjF1DJoNrFAFO98qg3ELkbR3GLaJBcs3JMMuofoaKBWgZ0YIxXDiuD87hDhW+DHA1lmIP74XzlIgqWw0a/0zWmNABrkVtQK/ywpcsCAAdksPmrVANL83j0xHwyo+kY67WtyW7H4ys2/rfARXQykk+6Bc/5C8yFW7pxw7lt/ChQgWSKlhiR+fy1m59euy9df+Cd3SO41UYUmLRamjDnhrLMbs8E14pnHn3oNve5D3qpJGuEyPC854Muhj0JHTw/wdVrCCQeNr4g7PlIsg7GqQj7Nn23iA7+NuPb8GXAaP2S9yCyR0o713GBW+uSSo8KCiiDhvEJ20XVB4BUw40/jU4bSVN3oKXInl/bB5VbVIMFhSB+sF/VODbbOaS4iafiVNQL2gV8= fuzakebito@ArchNTB
   '';
-  home.file.".ssh/authorized_keys".text = ''
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhJHNRy81Vn0+zc3HTNvimZx15XmATpYla97ggoawHF fuzakebito@DESKTOP-7TJKP6B
-  '';
-
   # Private key — encrypted in secrets.yaml, decrypted by sops-nix at
   # activation time directly to ~/.ssh/id_rsa with mode 0600. The shared
   # sops.age.keyFile / defaultSopsFile are set in home/opencode/default.nix;
