abfcc8ec556d99ca81f815fc04c26f2f90410658
HAYASHI Naoya <fuzakebito@fuzake.io>
2026-08-26 22:32:04 +0900

    feat(herdr): mirror tmux configuration

--- home/files.nix
+++ home/files.nix
@@ -8,6 +8,35 @@
     extraConfig = builtins.readFile ./files/tmux.conf;
   };
 
+  # Herdr mirrors the tmux keymap and status layout where supported.
+  xdg.configFile."herdr/config.toml".text = ''
+    [theme]
+    name = "terminal"
+
+    [theme.custom]
+    accent = "cyan"
+    active_row_bg = "black"
+    selection_bg = "black"
+
+    [terminal]
+    default_shell = "${pkgs.zsh}/bin/zsh"
+    new_cwd = "follow"
+
+    [keys]
+    prefix = "ctrl+z"
+    previous_tab = "prefix+shift+n"
+
+    [ui]
+    mouse_capture = true
+    prompt_new_tab_name = false
+    pane_outer_borders = false
+    pane_scrollbars = false
+    pane_gaps = false
+    tab_bar_position = "top"
+    tab_bar_right = [{ type = "hostname" }]
+    window_title = "{hostname}: {workspace}:{tab}"
+  '';
+
   # bashrc
   home.file.".bashrc".source = ./files/bashrc;
 
